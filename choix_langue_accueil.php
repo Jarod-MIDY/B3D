b00368df93e209f16f2dfd6e696290061ccb05c6
BibiLaMeduse <44779204+BibiLaMeduse@users.noreply.github.com>
2018-12-19 11:47:52 +0100

    Create choix_langue_accueil.php

---
+++ choix_langue_accueil.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="fr" dir="ltr">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="libs/bootstrap-4.1/css/bootstrap.min.css">
+    <link rel="stylesheet" href="styles/style_choixlangue.css">
+    <title>B3D</title>
+</head>
+<body>
+
+	<div class=container>
+	    
+	    <div class="row btnChoix">
+		  	<button type="button" class="col-3 col-sm-3 col-md-3 col-lg-3 col-xl-3 btn">Français</button>
+		  	<button type="button" class="col-3 col-sm-3 col-md-3 col-lg-3 col-xl-3 btn">English</button>
+		  	<button type="button" class="col-3 col-sm-3 col-md-3 col-lg-3 col-xl-3 btn">Deutsch</button>
+		  	<button type="button" class="col-3 col-sm-3 col-md-3 col-lg-3 col-xl-3 btn">Español</button>
+		</div>
+
+	</div>
+
+</body>
+</html>
