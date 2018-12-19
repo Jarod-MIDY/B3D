ac10bf3c16c315f671d654f7f6fd1deb9088255c
BibiLaMeduse <44779204+BibiLaMeduse@users.noreply.github.com>
2018-12-19 15:28:21 +0100

    Update choix_langue_accueil.php

--- choix_langue_accueil.php
+++ choix_langue_accueil.php
@@ -4,21 +4,26 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" href="libs/bootstrap-4.1/css/bootstrap.min.css">
-    <link rel="stylesheet" href="styles/style_choixlangue.css">
+    <link rel="stylesheet" href="styles/style_choix_langue.css">
     <title>B3D</title>
 </head>
 <body>
 
-	<div class=container>
+<div class=container>
 	    
-	    <div class="row btnChoix">
-		  	<button type="button" class="col-3 col-sm-3 col-md-3 col-lg-3 col-xl-3 btn">Français</button>
-		  	<button type="button" class="col-3 col-sm-3 col-md-3 col-lg-3 col-xl-3 btn">English</button>
-		  	<button type="button" class="col-3 col-sm-3 col-md-3 col-lg-3 col-xl-3 btn">Deutsch</button>
-		  	<button type="button" class="col-3 col-sm-3 col-md-3 col-lg-3 col-xl-3 btn">Español</button>
-		</div>
-
+	<div class="logo">
+		<img src="imgs/B3D_LogoWhite_petit.png" alt="Logo B3D" class="mx-auto d-block img-fluid">
 	</div>
+
+	<div class="btnChoix row d-flex justify-content-center">
+		<button type="button" class="btn">Français</button>
+		<button type="button" class="btn">English</button>
+		<button type="button" class="btn">Deutsch</button>
+		<button type="button" class="btn">Español</button>
+	</div>
+
+</div>
 
 </body>
 </html>
+
