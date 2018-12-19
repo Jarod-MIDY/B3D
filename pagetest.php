06818e65643cf70b6066aca0a173502dab1758a1
mohican19 <alban.belin@gmail.com>
2018-12-19 16:27:45 +0100

    code page entreprise.php + css

--- pagetest.php
+++ pagetest.php
@@ -1,5 +1,6 @@
 <?php include('parts/header.php'); ?>
 <div class="parallax">
+<?php include('societe.php'); ?>
 <?php include('produit.php'); ?>
 <?php include('parts/restercontact.php'); ?>
 </div>
--- societe.php
+++ societe.php
@@ -1,10 +1,25 @@
-<!DOCTYPE html>
-<html lang="en" dir="ltr">
-  <head>
-    <meta charset="utf-8">
-    <title></title>
-  </head>
-  <body>
+<section id="pageEntreprise" class="container bg-warning">
+  <h3 class=" text-center">L'Entreprise</h3>
 
-  </body>
-</html>
+    <div class="row justify-content-center m-1">
+      <div class="col-lg-4 col-md-4 col-sm-10">
+        <img class="img-fluid" src="imgs/tas_piquets.jpg" alt="">
+      </div>
+      <div class="col-lg-8 col-md-6 col-sm-12 d-flex align-items-center"><p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p></div>
+    </div>
+
+    <div id="zone2-entreprise" class="row justify-content-center m-1">
+      <div class="col-lg-8 col-md-6 col-sm-12  d-flex align-items-center"><p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p></div>
+      <div class="col-lg-4 col-md-4 col-sm-10">
+        <img class=" img-fluid" src="imgs/tas_piquets.jpg" alt="">
+      </div>
+    </div>
+
+  <div class="row justify-content-center m-1">
+    <div class="col-lg-4 col-md-4 col-sm-10 ">
+      <img class=" img-fluid" src="imgs/tas_piquets.jpg" alt="">
+    </div>
+    <div class="col-lg-8 col-md-6 col-sm-12 d-flex align-items-center"><p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p></div>
+  </div>
+
+</section>
