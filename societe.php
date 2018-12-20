62be64889e492d45f6a0c65152a37e1dd398d72c
mohican19 <alban.belin@gmail.com>
2018-12-20 14:56:40 +0100

    refonte page société et css lié

--- societe.php
+++ societe.php
@@ -3,40 +3,52 @@
 
 <section id="pageEntreprise" class="container">
   <h3 class="display-3 text-center">L'Entreprise</h3>
-
-    <div class="col-lg-8 col-md-12 col-sm-12 texte-entreprise">
-      <div class="row justify-content-center cadre bg-light">
-      <h5 class="">Historique</h5>
-      <div class="row justify-content-center">
-        <div class="col-lg-5 col-md-4 col-sm-10 d-flex align-items-center">
-          <img class="img-fluid" src="imgs/tas_piquets.jpg" alt="" >
+    <div class="row justify-content-start">
+      <div class="col-lg-8 col-md-12 col-sm-12">
+        <div class="row justify-content-center texte-entreprise cadre bg-light">
+          <h5 class="">Historique</h5>
+          <div class="row justify-content-center">
+            <div class="col-lg-5 col-md-4 col-sm-10 d-flex align-items-center">
+              <img class="img-fluid" src="imgs/tas_piquets.jpg" alt="" >
+            </div>
+            <div class="col-lg-7 col-md-6 col-sm-12">
+              <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+            </div>
+          </div>
         </div>
-        <div class="col-lg-5 col-md-6 col-sm-12"><p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p></div>
       </div>
     </div>
-</div>
-    <div class="col-lg-8 col-md-12 col-sm-12 texte-entreprise">
-      <div class="row justify-content-center cadre bg-light">
-      <h5 class="">Devpt Durable</h5>
-    <div id="zone2-entreprise" class=" row justify-content-center">
-      <div class="col-lg-7 col-md-6 col-sm-12  d-flex align-items-center"><p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p></div>
-      <div class="col-lg-5 col-md-4 col-sm-10">
+
+    <div class="row justify-content-end ">
+      <div class="col-lg-8 col-md-12 col-sm-12 ">
+      <div class="row justify-content-center texte-entreprise cadre bg-light">
+        <h5 class="">Devpt Durable</h5>
+        <div id="zone2-entreprise" class="row justify-content-center">
+          <div class="col-lg-7 col-md-6 col-sm-12  ">
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+          </div>
+          <div class="col-lg-5 col-md-4 col-sm-10 d-flex align-items-center">
+            <img class=" img-fluid" src="imgs/tas_piquets.jpg" alt="">
+          </div>
+        </div>
+      </div>
+    </div>
+    </div>
+
+    <div class="row justify-content-start">
+      <div class="col-lg-8  col-md-12 col-sm-12  ">
+    <div class="row justify-content-center texte-entreprise cadre bg-light">
+    <h5 class="">La chaine de production</h5>
+    <div class="row justify-content-center ">
+      <div class="col-lg-5 col-md-4 col-sm-10 d-flex align-items-center">
         <img class=" img-fluid" src="imgs/tas_piquets.jpg" alt="">
       </div>
+      <div class="col-lg-7 col-md-6 col-sm-12 d-flex align-items-center">
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. . Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+      </div>
+      </div>
     </div>
   </div>
-</div>
-
-  <div class="col-lg-8  col-md-12 col-sm-12 texte-entreprise ">
-    <div class="row justify-content-center cadre bg-light">
-    <h5 class="">La chaine de production</h5>
-  <div class="row justify-content-center m-1">
-    <div class="col-lg-5 col-md-4 col-sm-10 ">
-      <img class=" img-fluid" src="imgs/tas_piquets.jpg" alt="">
-    </div>
-    <div class="col-lg-7 col-md-6 col-sm-12 d-flex align-items-center"><p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. . Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p></div>
-  </div>
-</div>
 </div>
 </section>
 
