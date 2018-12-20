eebd7b23ad28c57bbc5fd50072cc40e4bebf6ce6
mohican19 <alban.belin@gmail.com>
2018-12-20 13:30:53 +0100

    refonte bloc contact et responsive design, css

--- societe.php
+++ societe.php
@@ -4,11 +4,11 @@
 <section id="pageEntreprise" class="container">
   <h3 class="display-3 text-center">L'Entreprise</h3>
 
-    <div class=" texte-entreprise">
+    <div class="col-lg-8 col-md-12 col-sm-12 texte-entreprise">
       <div class="row justify-content-center cadre bg-light">
       <h5 class="">Historique</h5>
-      <div class="row justify-content-start m-1">
-        <div class="col-lg-3 col-md-4 col-sm-10 d-flex align-items-center">
+      <div class="row justify-content-center">
+        <div class="col-lg-5 col-md-4 col-sm-10 d-flex align-items-center">
           <img class="img-fluid" src="imgs/tas_piquets.jpg" alt="" >
         </div>
         <div class="col-lg-5 col-md-6 col-sm-12"><p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p></div>
