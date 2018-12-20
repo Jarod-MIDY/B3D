5880cf897970ef499a3f3d267c8a64c0e718904a
BibiLaMeduse <44779204+BibiLaMeduse@users.noreply.github.com>
2018-12-20 14:29:08 +0100

    Update accueil.php
    intégration carrousel et "a propos"

--- accueil.php
+++ accueil.php
@@ -1,10 +1,66 @@
 <!DOCTYPE html>
-<html lang="en" dir="ltr">
-  <head>
+<html lang="fr" dir="ltr">
+<head>
     <meta charset="utf-8">
-    <title></title>
-  </head>
-  <body>
-ertyu
-  </body>
+    <title>B3D - Accueil</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="libs/bootstrap-4.1/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+  	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js"></script>
+ 	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js"></script>
+    <link rel="stylesheet" href="styles/style_accueil.css">
+</head>
+<body>
+
+<div id="container">
+<div id="demo" class="carousel slide" data-ride="carousel">
+
+  	<!-- The slideshow -->
+  	<div class="carousel-inner">
+    	<div class="carousel-item active">
+      		<img src="imgs/societe/redim/julienR.jpg" alt="Monsieur Procedes">
+    	</div>
+    	<div class="carousel-item">
+      		<img src="imgs/accueil/Acacia1.JPG" alt="Bois d'acacia coupé">
+    	</div>
+    	<div class="carousel-item">
+      		<img src="imgs/societe/tasBois1.jpg" alt="Un tas de bois">
+    	</div>
+    	<div class="carousel-item">
+      		<img src="imgs/accueil/Acacia3.JPG" alt="Bois d'acacia coupé">
+    	</div>
+    	<div class="carousel-item">
+      		<img src="imgs/accueil/Acacia7.JPG" alt="Bois d'acacia coupé">
+    	</div>
+    	<div class="carousel-item">
+      		<img src="imgs/produits/acacia/P1050506.JPG" alt="Piquets d'acacia">
+    	</div>
+    	<div class="carousel-item">
+      		<img src="imgs/accueil/Acacia9.JPG" alt="Bois d'acacia coupé">
+    	</div>
+    	<div class="carousel-item">
+      		<img src="imgs/accueil/Chataigner2.JPG" alt="Bois de chataigner coupé">
+    	</div>
+  	</div>
+
+  	<!-- Left and right controls -->
+  	<a class="carousel-control-prev" href="#demo" data-slide="prev">
+    	<span class="carousel-control-prev-icon"></span>
+  	</a>
+  	<a class="carousel-control-next" href="#demo" data-slide="next">
+    	<span class="carousel-control-next-icon"></span>
+  	</a>
+
+</div>
+</div>
+
+<div id="a-propos">
+	<div class="texte">
+		<h2>A propos</h2>
+		<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?</p>
+	</div>
+	<img id="img-a-propos" src="imgs/produits/acacia/redim/Colis1.JPG" alt="Tas de piquets en bois d'acacia">
+</div>
+
+</body>
 </html>
