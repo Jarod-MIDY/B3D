bc5c984121baba0a3eb9732a5f0e02b7252039d1
mohican19 <alban.belin@gmail.com>
2018-12-21 13:36:06 +0100

    debug accueil, rajout d'image et css

--- accueil.php
+++ accueil.php
@@ -1,33 +1,34 @@
 <?php include('parts/head.php'); ?>
 <?php include('parts/header.php'); ?>
-<section  id="container-accueil">
+<section  id="container-accueil" class="container">
+  <div class="row justify-content-center">
 <div id="demo" class="carousel slide" data-ride="carousel">
 
   	<!-- le slider -->
   	<div class="carousel-inner">
     	<div class="carousel-item active">
-      		<img src="imgs/societe/redim/julienR.jpg" alt="Monsieur Procedes">
+      		<img src="imgs/slider_accueil/julienR.jpg" alt="Monsieur Procedes">
     	</div>
     	<div class="carousel-item">
-      		<img src="imgs/accueil/Acacia1.JPG" alt="Bois d'acacia coupé">
+      		<img src="imgs/slider_accueil/Acacia1.JPG" alt="Bois d'acacia coupé">
     	</div>
     	<div class="carousel-item">
-      		<img src="imgs/societe/tasBois1.jpg" alt="Un tas de bois">
+      		<img src="imgs/slider_accueil/tasBois1.jpg" alt="Un tas de bois">
     	</div>
     	<div class="carousel-item">
-      		<img src="imgs/accueil/Acacia3.JPG" alt="Bois d'acacia coupé">
+      		<img src="imgs/slider_accueil/Acacia3.JPG" alt="Bois d'acacia coupé">
     	</div>
     	<div class="carousel-item">
-      		<img src="imgs/accueil/Acacia7.JPG" alt="Bois d'acacia coupé">
+      		<img src="imgs/slider_accueil/Acacia7.JPG" alt="Bois d'acacia coupé">
     	</div>
     	<div class="carousel-item">
-      		<img src="imgs/produits/acacia/P1050506.JPG" alt="Piquets d'acacia">
+      		<img src="imgs/slider_accueil/P1050506.JPG" alt="Piquets d'acacia">
     	</div>
     	<div class="carousel-item">
-      		<img src="imgs/accueil/Acacia9.JPG" alt="Bois d'acacia coupé">
+      		<img src="imgs/slider_accueil/Acacia9.JPG" alt="Bois d'acacia coupé">
     	</div>
     	<div class="carousel-item">
-      		<img src="imgs/accueil/Chataigner2.JPG" alt="Bois de chataigner coupé">
+      		<img src="imgs/slider_accueil/Chataigner2.JPG" alt="Bois de chataigner coupé">
     	</div>
   	</div>
 
@@ -39,13 +40,15 @@
 
 </div>
 </div>
-
-<div id="a-propos">
-	<div class="texte">
+<div id="a-propos" class="row">
+<div class="col-lg-6 col-sm-12 col-md-6">
 		<h2>A propos</h2>
 		<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?</p>
 	</div>
-	<img id="img-a-propos" src="imgs/produits/acacia/redim/Colis1.JPG" alt="Tas de piquets en bois d'acacia">
+  <div class="col-lg-6 col-sm-12 col-md-6 d-flex align-items-center">
+	<img id="img-a-propos" class="img-fluid" src="imgs/slider_accueil/Colis1.JPG" alt="Tas de piquets en bois d'acacia">
+</div>
+</div>
 </section>
 
 <?php include('parts/restercontact.php'); ?>
