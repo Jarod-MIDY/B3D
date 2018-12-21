d03c2e7d76bd55cd1e9c02c14f6da59b0806f647
mohican19 <alban.belin@gmail.com>
2018-12-21 09:12:48 +0100

    responsive page produits et css lié..

--- les-produits.php
+++ les-produits.php
@@ -1,12 +1,12 @@
 <?php include('parts/header.php'); ?>
-<div class="parallax">
+
 
 <section id ="lesProduits" class="container">
 
   <h3 class="display-3 text-center">Nos Produits</h3>
 <div class="row justify-content-center ">
-      <div class="col-lg-4 col-md-5 col-sm-10">
-        <div class="card m-2">
+      <div class="card-deck w-75">
+        <div class="card ">
           <div class="card-body text-center">
             <p><img class=" img-fluid" src="imgs/tas_piquets.jpg" alt="card image"></p>
             <h4 class="card-title">Nos piquets en chataignier</h4>
@@ -17,11 +17,7 @@
             <button class="btn-default" href="#">Voir le produit</button>
           </div>
         </div>
-
-      </div>
-
-      <div class="col-lg-4 col-md-5 col-sm-10">
-        <div class="card m-2">
+        <div class="card ">
           <div class="card-body text-center">
             <p><img class=" img-fluid" src="imgs/tas_piquets.jpg" alt="card image"></p>
             <h4 class="card-title">Nos piquets en acacia</h4>
@@ -35,38 +31,36 @@
       </div>
 </div>
 
-<div class="row justify-content-center">
-
-      <div class="col-lg-4 col-md-5 col-sm-10">
-        <div class="card m-2">
-        <div class="card-body text-center">
-          <p><img class=" img-fluid" src="imgs/tas_piquets.jpg" alt="card image"></p>
-          <h4 class="card-title">Nos piquets en pins</h4>
-          <h5><em></em></h5>
-          <p class="card-text">
-          </p>
-          <i class="" style="color: "></i>
-          <button class="btn-default" href="#">Voir le produit</button>
+<div class="row justify-content-center ">
+      <div class="card-deck w-75">
+        <div class="card ">
+          <div class="card-body text-center">
+            <p><img class=" img-fluid" src="imgs/tas_piquets.jpg" alt="card image"></p>
+            <h4 class="card-title">Nos piquets en pin</h4>
+            <h5><em></em></h5>
+            <p class="card-text">
+            </p>
+            <i class="" style="color: "></i>
+            <button class="btn-default" href="#">Voir le produit</button>
+          </div>
+        </div>
+        <div class="card ">
+          <div class="card-body text-center">
+            <p><img class=" img-fluid" src="imgs/tas_piquets.jpg" alt="card image"></p>
+            <h4 class="card-title">Bois de feu</h4>
+            <h5><em></em></h5>
+            <p class="card-text">
+            </p>
+            <i class="" style="color: "></i>
+            <button class="btn-default" href="#">Voir le produit</button>
+          </div>
         </div>
       </div>
-    </div>
+</div>
 
-      <div class="col-lg-4 col-md-5 col-sm-10">
-        <div class="card m-2">
-        <div class="card-body text-center">
-          <p><img class=" img-fluid" src="imgs/tas_piquets.jpg" alt="card image"></p>
-          <h4 class="card-title">Bois de feu</h4>
-          <h5><em></em></h5>
-          <p class="card-text">
-          </p>
-          <i class="" style="color: "></i>
-          <button class="btn-default" href="#">Voir le produit</button>
-        </div>
-      </div>
-    </div>
-    </div>
+
 </section>
 
 <?php include('parts/restercontact.php'); ?>
-</div>
+
 <?php include('parts/footer.php'); ?>
