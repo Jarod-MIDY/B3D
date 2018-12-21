a37fe507e7405abf8e0846f7f9897416442df743
mohican19 <alban.belin@gmail.com>
2018-12-21 10:24:31 +0100

    construction page produit et css

--- produit.php
+++ produit.php
@@ -1,14 +1,53 @@
 <?php include('parts/header.php'); ?>
-<div class="parallax p-5">
-
-<section id="produit" class="container cadre bg-danger mb-5">
-  <h3 class=" text-center">Nom du produit</h3>
-  <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. </p>
-  <div class="row">
-    <div class="col-lg-6"></div>
+<section id="produit" class="container">
+  <h3 class="display-3 text-center">Nom du produit</h3>
+  <p class="text-center"><em>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</em> </p>
+  <div class="row justify-content-end">
+    <div class="col-lg-1"></div>
+    <div class="col-lg-11 col-sm-12 col-md-12">
+      <div class="row ">
+        <div class="col-lg-8 col-sm-12 col-md-8 p-0">
+          <div class="cadre bg-success">
+            <h5 class="display-5"> Le chataignier</h5>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. . Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+          </div>
+          <div class="cadre bg-success mt-2">
+          <h5 class="display-5"> Qualités et avantages</h5>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+          </div>
+        </div>
+        <div class="col-lg-4 col-sm-12 col-md-4 d-flex align-items-center">
+          <img class=" img-fluid" src="imgs/tas_piquets.jpg" alt="card image">
+        </div>
+      </div>
+      <div class="row">
+        <div class="cadre bg-success">
+          <h5 class="display-5"> Les usages</h5>
+          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+        </div>
+      </div>
+      <div class="row">
+        <div class="col-lg-4 col-sm-12 col-md-4">
+          <img class=" img-fluid" src="imgs/tas_piquets.jpg" alt="card image">
+        </div>
+        <div class="col-lg-4 col-sm-12 col-md-4">
+          <img class=" img-fluid" src="imgs/tas_piquets.jpg" alt="card image">
+        </div>
+        <div class="col-lg-4 col-sm-12 col-md-4">
+          <img class=" img-fluid" src="imgs/tas_piquets.jpg" alt="card image">
+        </div>
+      </div>
+    </div>
+  </div>
+  <div class="row justify-content-center">
+    <div class="col-12 cadre bg-light text-center p-1">
+      <h5 class="display-5 ">Fiche technique</h5>
+      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+      <button class="btn-default" target="_blank" href="#" title="télécharger la fiche technique au format pdf, ouvre dans un nouvel onglet"><i class="fas fa-download"></i> Télécharger la fiche technique</button>
+    </div>
   </div>
 </section>
 
 <?php include('parts/restercontact.php'); ?>
-</div>
+
 <?php include('parts/footer.php'); ?>
