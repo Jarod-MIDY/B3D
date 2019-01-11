09e0e04d1f3056adaeb0d8285b44a4c794a75c7d
Jarod <jarod.midy@gmail.com>
2019-01-11 11:58:35 +0100

    ajout séparateur | sur choix langue + flex

--- choix_langue.php
+++ choix_langue.php
@@ -1,14 +1,17 @@
-<div id="choix-langue" class="container-fluid p-5">
+<div id="choix-langue" class="container-fluid p-5 d-flex justify-content-center align-items-center">
 <div class="">
 	<div class="logo">
 		<img src="imgs/B3D_LogoWhite_petit.png" alt="Logo B3D" class="mx-auto d-block img-fluid">
 	</div>
 
-	<div class="btnChoix row d-flex justify-content-center">
-		<button type="button" class="btn scroll-up">Français</button>
-		<button type="button" class="btn scroll-up">English</button>
-		<button type="button" class="btn scroll-up">Deutsch</button>
-		<button type="button" class="btn scroll-up">Español</button>
+	<div class="btnChoix row">
+		<button type="button" class="btn scroll-up">FRANÇAIS</button>
+		<p class="btnsep">|</p>
+		<button type="button" class="btn scroll-up">ENGLISH</button>
+		<p class="btnsep">|</p>
+		<button type="button" class="btn scroll-up">DEUTSCH</button>
+		<p class="btnsep">|</p>
+		<button type="button" class="btn scroll-up">ESPAÑOL</button>
 	</div>
 </div>
 </div>
