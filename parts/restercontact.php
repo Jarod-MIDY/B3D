346aa7e1fa7fa9a73257a72a0c4004668aeb7762
Jarod <jarod.midy@gmail.com>
2019-01-21 12:23:21 +0100

    css reston en contact

--- parts/restercontact.php
+++ parts/restercontact.php
@@ -1,22 +1,19 @@
 <section id="container-contact-nous" class="container">
 	<div  class="row justify-content-center">
 	<div  id="contactNous"  class="col-lg-12 col-md-12 col-sm-12">
-		<h4 class="display-4  text-center">Restons en contact</h5>
 				<div class="row">
 					<div class="col-lg-6 col-md-6 col-sm-12  text-center">
-						<iframe width="425" height="350" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="https://maps.yahoo.com/e/?lat=44.34926359035272&amp;lon=0.07158279418945312&amp;q=l%27intendant%2047700%20Poussignac&amp;bb=44.37896245%2C-0.00437737%2C44.31954967%2C0.14737129&amp;addr=l%27intendant%2047700%20Poussignac"></iframe>
+						<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d5706.59330503512!2d0.07091420449699201!3d44.344956780262315!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x12aa7c208889a78f%3A0x8662b027363dce5a!2sL&#39;Intendant%2C+47700+Poussignac!5e0!3m2!1sfr!2sfr!4v1545297392869" class="col-md-12" height="450" frameborder="0" style="border:0" allowfullscreen></iframe>
 					</div>
 					<div class="col-lg-6 col-md-6 col-sm-12  text-center ">
-						<h4 class="">Adresse et horaires</h4>
-						<div class="row justify-content-center text-left">
+						<h4 class="display-4  text-center">Restons en contact</h4>
+						<div class="row text-left">
 							<ul class="adresse-contact">
-								<li>L'intendant</li>
-								<li>47700 Poussignac</li>
-							</ul>
-							<ul class="adresse-contact">
-								<li>9H-12H 14H-17H</li>
-								<li>du lundi au vendredi</li>
-								<li> telephone ??</li>
+								<li><span class="underline">Télephone :</span> 06-74-90-65-96</li>
+								<li><span class="underline">Mail :</span> julien.procedes@orange.fr</li>
+								<li><span class="underline">Adresse :</span> Julien PROCEDES</li>
+								<li>B3D L'intendant 47700 Poussignac</li>
+								<li><span class="underline">Horaires :</span> Du lundi au vendredi 8h - 12h 14h - 18h et samedi 9h - 12h</li>
 							</ul>
 						</div>
 						<a href="contact.php" target="" title="ouvre le formulaire de contact"><button type="button" class="btn btn-contactNous"><i class="fas fa-chevron-circle-right"></i> Me contactez</button></a>
