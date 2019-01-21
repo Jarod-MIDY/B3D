39ba2373e1000dbf08029ebf743f300deb020884
Noémie <noemielafont19@gmail.com>
2019-01-21 15:29:26 +0100

    ordre des appels revu

--- parts/head.php
+++ parts/head.php
@@ -9,11 +9,11 @@ session_start();
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<title>B3D</title>
-	<link href="libs/bootstrap-4.1/css/bootstrap.min.css" rel="stylesheet"><!-- en local -->
-	<script src="scripts/jquery-3.3.1.min.js"></script><!-- en local -->
-	<script src="libs/bootstrap-4.1/js/bootstrap.bundle.min.js"></script><!-- en local -->
-	<script src="libs/bootstrap-4.1/js/bootstrap.min.js"></script><!-- en local -->
-	<link href="libs/fontawesome-free-5.5.0-web/css/all.css" rel="stylesheet"><!-- en local -->
-	<script defer src="libs/fontawesome-free-5.5.0-web/js/all.js"></script><!-- en local -->
-	<link rel="stylesheet" href="styles/master.css">
+	<link href="libs/bootstrap-4.1/css/bootstrap.min.css" rel="stylesheet">
+	<script src="libs/bootstrap-4.1/js/bootstrap.min.js"></script>
+	<script src="scripts/jquery-3.3.1.min.js"></script>
+	<script src="libs/bootstrap-4.1/js/bootstrap.bundle.min.js"></script>
+	<link href="libs/fontawesome-free-5.5.0-web/css/all.css" rel="stylesheet">
+	<script defer src="libs/fontawesome-free-5.5.0-web/js/all.js"></script>
+  	<link rel="stylesheet" href="styles/master.css">
 </head>
