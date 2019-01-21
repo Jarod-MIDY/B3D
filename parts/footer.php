77ba7bfe1f45329c53b0f1a0e1bd80b8e705efe6
Jarod <jarod.midy@gmail.com>
2019-01-21 10:17:49 +0100

    indentation et correction footer

--- parts/footer.php
+++ parts/footer.php
@@ -2,11 +2,16 @@
 
 		<div class="row justify-content-center">
 			<div class="col-lg-4 col-md-10 col-sm-10 text-center">
-					<div><a href="#header" class="scroll-up"><i class="fas fa-angle-up fa-3x" style="color: white"></i></a></div>
-					<div><a href="#">B3D</a></div>
-					<div class=""><p>Copyright © 2018-Tout droit Reservé ??</p></div>
-					<p><u><a class="link" href="#">Plan de site</a></u> |
-						<u><a class="link" href="">Mentions légales</a></u></p>
+					<div>
+						<a href="#header" class="scroll-up"><i class="fas fa-angle-up fa-3x" style="color: white"></i></a>
+					</div>
+					<div>
+						<a href="#">B3D</a>
+					</div>
+					<div class="">
+						<p>Copyright © 2018-Tout droit Reservé ??</p>
+					</div>
+					<p><a class="link" href="#">Plan de site</a> | <a class="link" href="">Mentions légales</a></p>
 			</div>
 		</div>
 	</footer>
