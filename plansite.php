272372a9362a114c304cb6fdce31d631e8278e84
Jarod <jarod.midy@gmail.com>
2019-01-21 13:47:53 +0100

    plan de site vide + rename index

---
+++ plansite.php
@@ -0,0 +1,9 @@
+<?php include('parts/head.php'); ?>
+<?php include('parts/header.php'); ?>
+
+<section>
+  
+</section>
+
+<?php include('parts/restercontact.php'); ?>
+<?php include('parts/footer.php'); ?>
