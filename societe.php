e2d60adbc199272d0c4a40dd1ca46d72ef34c8c2
mohican19 <alban.belin@gmail.com>
2019-01-28 13:45:04 +0100

    résolution de conflit page entreprise

--- societe.php
+++ societe.php
@@ -4,28 +4,18 @@
 <section id="pageEntreprise" class="container">
   <h3 class="display-3 text-center">L'Entreprise</h3>
     <div class="row justify-content-start">
-<<<<<<< Updated upstream
+
       <div class="col-lg-9 col-md-12 col-sm-12">
         <div class="row justify-content-center bloc-texte cadre">
-=======
-     
-      <div class="col-lg-9 col-md-10 col-sm-12">
-        <div class="row justify-content-center texte-entreprise cadre">
-<<<<<<< Updated upstream
 
-=======
->>>>>>> Stashed changes
->>>>>>> Stashed changes
           <div class="row justify-content-center">
             <div class="col-lg-5 col-md-4 col-sm-10 d-flex align-items-center">
               <img class="img-fluid  mx-0 my-0" src="imgs/tas_piquets.jpg" alt="" >
             </div>
             <div class="col-lg-7 col-md-6 col-sm-12">
-<<<<<<< Updated upstream
+
               <h4 class="mx-auto">Historique</h4>
-=======
-             <h4 class="mx-auto">Historique</h4>
->>>>>>> Stashed changes
+
               <p>Notre société est située à Poussignac dans le Lot-et-Garonne aux portes du massif landais et au plus prés de la ressource forestière.<br/> Dans les années 80, mon père y a construit une scierie de pin maritime fermée en 2015.<br/> En 2018, après avoir travaillé dans différents organismes et sociétés de la filière bois, j’ai choisi de revenir sur le site familial pour installer une unité de fabrication de piquets bois.<br/> La présence d’une diversité d’essences de bois locaux a orienté mon choix sur un outil de transformation souple et flexible permettant de produire des piquets acacia, pin et châtaigner de différentes dimensions.</p>
             </div>
           </div>
@@ -34,24 +24,14 @@
     </div>
 
     <div class="row justify-content-end ">
-<<<<<<< Updated upstream
+
       <div class="col-lg-9 col-md-12 col-sm-12 ">
       <div class="row justify-content-center bloc-texte cadre">
         <div id="zone2-entreprise" class="row justify-content-center">
-=======
-      <div class="col-lg-9 col-md-10 col-sm-12 ">
-      <div class="row justify-content-center texte-entreprise cadre">
-<<<<<<< Updated upstream
-        <div id="zone2-entreprise" class="rjustify-content-center">
->>>>>>> Stashed changes
+
           <div class="col-lg-7 col-md-6 col-sm-12  ">
             <h4 class="mx-auto">Développement durable</h4>
-=======
-        
-        <div id="zone2-entreprise" class="row justify-content-center">
-          <div class="col-lg-7 col-md-6 col-sm-12  ">
-           <h4 class="mx-auto">Développement durable</h4>
->>>>>>> Stashed changes
+
             <p><em>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</em></p>
           </div>
           <div class="col-lg-5 col-md-4 col-sm-10 d-flex align-items-center mx-0 my-0">
@@ -63,28 +43,19 @@
     </div>
 
     <div class="row justify-content-start">
-<<<<<<< Updated upstream
+
       <div class="col-lg-9  col-md-12 col-sm-12  ">
         <div class="row justify-content-center bloc-texte cadre">
-=======
-      <div class="col-lg-9  col-md-10 col-sm-12  ">
-        <div class="row justify-content-center texte-entreprise cadre">
-<<<<<<< Updated upstream
-=======
-          
->>>>>>> Stashed changes
->>>>>>> Stashed changes
+
+
           <div class="row justify-content-center ">
             <div class="col-lg-5 col-md-4 col-sm-10 d-flex align-items-center mx-0 my-0">
               <img class=" mx-0 my-0 img-fluid" src="imgs/tas_piquets.jpg" alt="">
             </div>
-<<<<<<< Updated upstream
+
             <div class="col-lg-7 col-md-6 col-sm-12  align-items-center">
               <h4 class="">La chaine de production</h4>
-=======
-            <div class="col-lg-7 col-md-6 col-sm-12 d-flex align-items-center">
-             <h4 class="mx-auto">La chaine de production</h4>
->>>>>>> Stashed changes
+
               <p>Suivant l’essence et le diamètre des bois transformés deux opérations sont réalisées.<br/> Une opération de planage permettant d’apporter un état de surface raboté et lisse aux bois ronds ou une opération de fente permettant de suivre le fil du bois et conserver la résistance mécanique du bois et éviter les déformations de séchage.<br/> En mesure de précaution sanitaire une opération d’écorçage est réalisée pour chaque essence de bois. Cette opération permet d’éviter de transporter d’éventuels parasites comme des insectes xylophages sur le site de la clientèle.<br/> La satisfaction client est notre objectif premier et un contrôle de la qualité du produit est réalisé à chaque étape de transformation.</p>
             </div>
           </div>
