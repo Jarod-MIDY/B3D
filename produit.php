9913800efc9603336acfb0657c6831f5d50f7ec6
mohican19 <alban.belin@gmail.com>
2019-01-30 10:49:30 +0100

    pagination page produit (dont inclusion tableau_chataignier.php), rajout dans css tableau, ajout du fichier tableau....)

--- produit.php
+++ produit.php
@@ -8,22 +8,23 @@
     <div class="col-lg-11 col-sm-12 col-md-12">
       <div class="row ">
         <div class="col-lg-8 col-sm-12 col-md-8 p-0">
-          <div class="cadre bg-success">
+          <div class="cadre bloc-texte">
             <h5 class="display-5"> Le chataignier</h5>
             <p><em>Castanea sativa</em> , le chataignier, est un arbre à croissance rapide en effet il atteint sa taille adulte entre 20 et 30 ans. Sa faculté de régénérescence permet une culture en taillis qui permet une gestion forêstiére raisonnée et durable. Originaire de la zone méditérrannéenne, l'homme l'a répandu en Europe pour ses multiples usages et sa facilité de culture.</p>
+            <p>Grâce à la présence d’acide tanique, les piquets n’ont pas besoin d’être traités et ne demandent pas d’entretien. Ils sont protégés contre l’humidité. L’utilisation de piquets en châtaignier ne nuit donc pas à l’environnement.</p>
           </div>
-          <div class="cadre bg-success mt-2">
+          <!-- <div class="cadre bloc-texte mt-2">
           <h5 class="display-5"> Qualités et avantages</h5>
             <p>
             Grâce à la présence d’acide tanique, les piquets n’ont pas besoin d’être traités et ne demandent pas d’entretien. Ils sont protégés contre l’humidité. L’utilisation de piquets en châtaignier ne nuit donc pas à l’environnement.</p>
-          </div>
+          </div> -->
         </div>
         <div class="col-lg-4 col-sm-12 col-md-4 d-flex align-items-center">
           <img class=" img-fluid" src="http://s3.amazonaws.com/everystockphoto/fspid30/71/72/96/5/emst-gelderland-holland-7172965-h.jpg" alt="card image">
         </div>
       </div>
       <div class="row">
-        <div class="cadre bg-success">
+        <div class="cadre bloc-texte">
           <h5 class="display-5"> Les usages</h5>
           <p>Le chataignier est depuis longtemps exploité pour ses châtaignes, consommée par l'homme ou les cochons menés en pâturage dans la forêt qui est une pratique encore effective en Corse ou Espagne par exemple.
           Il est utilisé pour la charpente, le parquet... Le bois dur était utilisé autrefois dans la tonnellerie (fabrique de barrique de vins). Il donne un chauffage moyen.
@@ -45,11 +46,16 @@
     </div>
   </div>
   <div class="row justify-content-center">
-    <div class="col-12 cadre bg-light text-center p-1">
+    <div class="col-12 cadre bloc-texte text-center p-1">
       <h5 class="display-5 ">Fiche technique</h5>
-      <p> Les piquets sont écorcés et pointés, mais gardent pour le reste leur forme naturelle. Les piquets ne sont donc pas toujours droits.</br>
-      Nos piquets de chataigniers sont disponibles en 240cm, 280cm, 400cm (pour d'autres dimensions nous contacter).
+      <p> L’ensemble de nos produits châtaigner sont écorcés pour apporter plus d’esthétique et de propreté
+          aux piquets mais surtout pour éviter de transporter des parasites sur le site de la clientèle.</br>
+          Nos piquets acacia sont fendus ou planés et une pointe carrée d’une surface comprise entre 1 et 1,5
+          cm² est réalisée gros bout.</br>
+          L’opération de fente permet au bois de conserver sa résistance mécanique et limiter les
+          déformations de séchage.
       </p>
+      <?php include ('tableau_chataignier.php'); ?>
       <button class="btn-default" target="_blank" href="#" title="télécharger la fiche technique au format pdf, ouvre dans un nouvel onglet"><i class="fas fa-download"></i> Télécharger la fiche technique</button>
     </div>
   </div>
---
+++ tableau_chataignier.php
@@ -0,0 +1,19 @@
+<?php
+echo '<table class="tg justify-content-center">
+  <tr>
+    <th class="tg-0lax" colspan="2">Longueurs</th>
+    <th class="tg-0lax">2 m</th>
+    <th class="tg-0lax">2 m 30</th>
+    <th class="tg-0lax">2 m 50</th>
+  </tr>
+  <tr>
+    <td class="tg-0lax" rowspan="2"> Périmétres fin bout</td>
+    <td class="tg-0lax">24/29 cm</td>
+    <td class="tg-0lax" colspan="3">130</td>
+  </tr>
+  <tr>
+    <td class="tg-0lax">28cm ret +</td>
+    <td class="tg-0lax" colspan="3">110</td>
+  </tr>
+</table>'
+ ?>
