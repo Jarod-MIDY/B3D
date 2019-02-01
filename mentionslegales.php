306fa17dfd3327a6f2011bcfc8c148b77642991a
Jarod <jarod.midy@gmail.com>
2019-02-01 15:54:28 +0100

    truc

--- mentionslegales.php
+++ mentionslegales.php
@@ -3,10 +3,10 @@
 
 <section class="container w-75">
 	<br><br>
-	<h1 class="text-center display-4">Conditions générales d'utilisation du site B3D</h1><br>
+	<h2 class="text-center display-4">Conditions générales d'utilisation du site B3D</h2><br>
 
 	<section class="row cadreCGU"><br>
-	<h2 class="w-100">ARTICLE 1 : Objet      <hr></h2><br>
+	<h5 class="w-100">ARTICLE 1 : Objet      <hr></h5><br>
 	<p>Les présentes « conditions générales d'utilisation » ont pour objet l'encadrement juridique de l’utilisation du site B3D et de ses services.<br>
 		Ce contrat est conclu entre :<br>
 		Le gérant du site internet, ci-après désigné « l’Éditeur »,<br>
@@ -15,7 +15,7 @@
 		Les conditions générales d'utilisation doivent être acceptées par tout Utilisateur, et son accès au site vaut acceptation de ces conditions.</p>
 	</section>
 	<section class="row cadreCGU"><br>
-		<h2 class="w-100">ARTICLE 2 : Mentions légales      <hr></h2><br>
+		<h5 class="w-100">ARTICLE 2 : Mentions légales      <hr></h5><br>
 		<p>Pour les personnes morales :<br><br>
 			Le site B3D est édité par la société B3D, SARL à associé unique au capital de 10 000 €, dont le siège social est situé à "L'intendant" 47700 Poussignac.<br>
 			La société est représentée par Mr Procedes Julien, Marc.<br><br>
@@ -24,25 +24,25 @@
 			La société est représentée par Mr Procedes Julien, Marc.<br><br></p>
 	</section>
     <section class="row cadreCGU"><br>
-        <h2 class="w-100">ARTICLE 3 : accès aux services      <hr></h2><br>
+        <h5 class="w-100">ARTICLE 3 : accès aux services      <hr></h5><br>
             <p>Tout Utilisateur ayant accès a internet peut accéder gratuitement et depuis n’importe où au site. Les frais supportés par l’Utilisateur pour y accéder (connexion internet, matériel informatique, etc.) ne sont pas à la charge de l’Éditeur.<br>Le site et ses différents services peuvent être interrompus ou suspendus par l’Éditeur, notamment à l’occasion d’une maintenance, sans obligation de préavis ou de justification.</p>
     </section>
 
     <section class="row cadreCGU"><br>
-            <h2 class="w-100">ARTICLE 4 : Responsabilité de l’Éditeur      <hr></h2><br>
+            <h5 class="w-100">ARTICLE 4 : Responsabilité de l’Éditeur      <hr></h5><br>
             <p>Tout dysfonctionnement du serveur ou du réseau ne peut engager la responsabilité de l’Éditeur.<br>
             De même, la responsabilité du site ne peut être engagée en cas de force majeure ou du fait imprévisible et insurmontable d'un tiers.<br>
             Le site B3D s'engage à mettre en œuvre tous les moyens nécessaires pour garantir la sécurité et la confidentialité des données. Toutefois, il n’apporte pas une garantie de sécurité totale.<br>
             L’Éditeur se réserve la faculté d’une non-garantie de la fiabilité des sources, bien que les informations diffusées su le site soient réputées fiables.</p>
     </section>
     <section class="row cadreCGU"><br>
-            <h2 class="w-100">ARTICLE 5 : Propriété intellectuelle      <hr></h2><br>
+            <h5 class="w-100">ARTICLE 5 : Propriété intellectuelle      <hr></h5><br>
             <p>Les contenus du site B3D (logos, textes, éléments graphiques, vidéos, etc.) son protégés par le droit d’auteur, en vertu du Code de la propriété intellectuelle.<br>
             L’Utilisateur devra obtenir l’autorisation de l’éditeur du site avant toute reproduction, copie ou publication de ces différents contenus.<br>
             Ces derniers peuvent être utilisés par les utilisateurs à des fins privées ; tout usage commercial est interdit.<br></p>
     </section>
     <section class="row cadreCGU"><br>
-            <h2 class="w-100">ARTICLE 6 : Données personnelles      <hr></h2><br>
+            <h5 class="w-100">ARTICLE 6 : Données personnelles      <hr></h5><br>
             <p>L’Utilisateur doit obligatoirement fournir son adresse é-mail pour procéder à une demande de contact ou de renseignements. <br>
 
             L’adresse électronique (e-mail) de l’utilisateur pourra notamment être utilisée par le site B3D pour la communication d’informations diverses et la gestion des commandes et des réponses par e-mail.<br>
@@ -59,16 +59,16 @@
             </ul></p>
     </section>
     <section class="row cadreCGU"><br>
-            <h2 class="w-100">ARTICLE 7 : Liens hypertextes      <hr></h2><br>
+            <h5 class="w-100">ARTICLE 7 : Liens hypertextes      <hr></h5><br>
             <p>Les domaines vers lesquels mènent les liens hypertextes présents sur le site n’engagent pas la responsabilité de l’Éditeur de B3D, qui n’a pas de contrôle sur ces liens.<br>
             Il est possible pour un tiers de créer un lien vers une page du site B3D sans autorisation expresse de l’éditeur.</p>
     </section>
     <section class="row cadreCGU"><br>
-            <h2 class="w-100">ARTICLE 8 : Évolution des conditions générales d’utilisation      <hr></h2><br>
+            <h5 class="w-100">ARTICLE 8 : Évolution des conditions générales d’utilisation      <hr></h5><br>
             <p>Le site B3D se réserve le droit de modifier les clauses de ces conditions générales d’utilisation à tout moment et sans justification.</p>
     </section>
     <section class="row cadreCGU"><br>
-            <h2 class="w-100">ARTICLE 9 : Droit applicable et juridiction compétente      <hr></h2><br>
+            <h5 class="w-100">ARTICLE 9 : Droit applicable et juridiction compétente      <hr></h5><br>
             <p>Le présent contrat dépend de la législation française.
             En cas de litige non résolu à l’amiable entre l’Utilisateur et l’Éditeur, les tribunaux de Cahors sont compétents pour régler le contentieux.</p>
     </section>
