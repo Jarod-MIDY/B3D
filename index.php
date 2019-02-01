06ca6b793c622b0898417628a1a9e52567e2e6db
Jarod <jarod.midy@gmail.com>
2019-02-01 14:06:30 +0100

    BDD latin swedish + trad dans index

--- index.php
+++ index.php
@@ -3,7 +3,7 @@ include('parts/head.php');
 include('parts/form.php');
 include('choix_langue.php');
 include('parts/header.php');
-$req = $db->prepare('SELECT '.$_SESSION['langue'].', image FROM b3d_altimages LIMIT 8');
+$req = $db->prepare('SELECT '.$_SESSION['langue'].', image FROM dwb3d1_altimages LIMIT 8');
 $req->execute();
  ?>
 
@@ -25,30 +25,6 @@ $req->execute();
         </div>';
       }
        ?>
-             <!-- <div class="carousel-item active">
-         <img src="imgs/slider_accueil/tasBois1.jpg" alt="Un tas de bois">
-       </div>
-       <div class="carousel-item">
-         <img src="imgs/slider_accueil/Acacia1.jpg" alt="Bois d'acacia coupé">
-       </div>
-       <div class="carousel-item">
-         <img src="imgs/slider_accueil/julienR.jpg" alt="Monsieur Procedes">
-       </div>
-       <div class="carousel-item">
-         <img src="imgs/slider_accueil/Acacia3.jpg" alt="Bois d'acacia coupé">
-       </div>
-       <div class="carousel-item">
-         <img src="imgs/slider_accueil/Acacia7.jpg" alt="Bois d'acacia coupé">
-       </div>
-       <div class="carousel-item">
-         <img src="imgs/slider_accueil/P1050506.jpg" alt="Piquets d'acacia">
-       </div>
-       <div class="carousel-item">
-         <img src="imgs/slider_accueil/Acacia9.jpg" alt="Bois d'acacia coupé">
-       </div>
-       <div class="carousel-item">
-         <img src="imgs/slider_accueil/Chataigner2.jpg" alt="Bois de chataigner coupé">
-       </div> -->
     </div>
     <!-- controle du slider -->
     <a class="carousel-control-prev" href="#demo" data-slide="prev">
@@ -56,8 +32,6 @@ $req->execute();
     <a class="carousel-control-next" href="#demo" data-slide="next">
     <span class="carousel-control-next-icon"></span></a>
   </div>
-</section>
-<section id="container-accueil" class="">
 </section>
 <section id="container-accueil" class="">
   <div class="container">
@@ -69,8 +43,12 @@ $req->execute();
                 <img class="img-fluid" src="imgs/accueil_01.jpg" alt="Tas de piquets en bois d'acacia" >
               </div>
             <div class="col-lg-7 col-md-6 col-sm-12">
-            <p>Fort de 20 ans d’expérience dans la ﬁlière bois, j’ai souhaité faire un retour aux sources en m’installant dans l’ancienne scierie familiale située au plus près de la ressource forestière.<br>Je me suis spécialisé dans la fabrication de piquets et rondins d’acacia, pins ou châtaignier.<br>Mon outil de production souple et flexible permet de produire des piquets ou rondins fendus ou ronds de différentes dimensions.<br>Destinés aux vignerons, paysagistes, arboriculteurs, ou tout simplement aux particuliers désireux de créer une clôture ou un aménagement de jardin, les piquets et rondins B3D sont une valeur sûre.
-            </p>
+              <?php
+              $req = $db->prepare('SELECT '.$_SESSION['langue'].' FROM dwb3d1_generaltexte WHERE id=1');
+              $req->execute();
+              $data = $req->fetch();
+              echo '<p>'.$data[$_SESSION["langue"]].'</p>';
+               ?>
             </div>
           </div>
         </div>
@@ -83,8 +61,12 @@ $req->execute();
       <div class="row justify-content-center bloc-texte">
         <div class="row justify-content-center">
           <div class="col-lg-7 col-md-6 col-sm-12">
-            <p>Nos piquets et rondins sont produits à partir de grumes exploitées dans un rayon de 70 km. Le pin provient de coupes d’éclaircies plantées après la tempête de 1999. Ces bois juvéniles utilisés généralement pour la mise en place de filets anti-grêle ou le palissage comportent une très faible proportion de bois de cœur leurs donnant une très bonne aptitude au traitement autoclave. Nos grumes d’acacia comportent une très faible quantité d’aubier et des cernes d’accroissement serrées conférant au bois une excellente durabilité naturelle et une très bonne résistance mécanique. L’ensemble de nos bois proviennent de coupes gérées durablement et bénéficie généralement de la certification PEFC.
-            </p>
+            <?php
+            $req = $db->prepare('SELECT '.$_SESSION['langue'].' FROM dwb3d1_generaltexte WHERE id=2');
+            $req->execute();
+            $data = $req->fetch();
+            echo '<p>'.$data[$_SESSION["langue"]].'</p>';
+             ?>
           </div>
             <div class="col-lg-5 col-md-4 col-sm-10 d-flex align-items-center">
               <img class="img-fluid" src="imgs/accueil_02.jpg" alt="Tas de piquets en bois d'acacia" >
